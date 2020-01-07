61c47ce0433d7ad965b239cf59c9b0e22d279956
caodawei <caodawei2001@163.com>
2020-01-07 11:24:03 +0800

    1

---
+++ wc.py
@@ -0,0 +1,12 @@
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
+
+if __name__ == '__main__':
+    conf = SparkConf()
+    conf.setMaster("local").setAppName("wc")
+    sc = SparkContext(conf=conf)
+    rdd = sc.textFile("words.txt")
+    rdd = rdd.flatMap(lambda s: s.split(" "))
+    rdd = rdd.map(lambda s: (s, 1))
+    rdd = rdd.reduceByKey(lambda v1, v2: v1 + v2)
+    rdd.foreach(lambda s: print(s))
